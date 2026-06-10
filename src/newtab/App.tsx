91b4f93c7fc25efb666b339f73c5cead26f39cfc
VicoHu <vicohu@163.com>
2026-06-10 23:28:48 +0800

    feat: 初始化项目骨架 (Vite + React + TS + Chrome Extension MV3)

---
+++ src/newtab/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Octane</h1>
+      <p>书签 + 笔记 + 安全</p>
+    </div>
+  );
+};
+
+export default App;
